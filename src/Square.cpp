412a999fee2613153ce0929fcfd6aef5b2361686
Charline <charli.le.pape@gmail.com>
2023-03-15 16:50:00 +0100

    add collision

--- src/Square.cpp
+++ src/Square.cpp
@@ -15,11 +15,18 @@ void Square::updatePosition(p6::Context& ctx) {
     
     std:: cout << ctx.current_canvas_width() << std::endl;
     std:: cout << ctx.current_canvas_height() << std::endl;
- 
 
-    this->speed.x += this->acc.x*dt;
-	this->speed.y += this->acc.y*dt;
-	this->center.x += this->speed.x*dt;
-	this->center.y += this->speed.y*dt;
+    if(abs(this->center.x + this->radius)<2 && abs(this->center.y + this->radius)<1 ){
+        this->speed.x += this->acc.x*dt;
+        this->speed.y += this->acc.y*dt;
+        this->center.x += this->speed.x*dt;
+        this->center.y += this->speed.y*dt;
+    }
+    else {
+        
+        this->center.x = 0.9;
+        this->center.y = -0.5;
+    }
+
 
 }
