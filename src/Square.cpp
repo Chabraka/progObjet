7c12331aa281f504ba346102c42290e7e02ce160
Juliette-J <juliette.jeannin@hotmail.fr>
2023-03-31 15:26:56 +0200

    add ImGui window

--- src/Square.cpp
+++ src/Square.cpp
@@ -13,64 +13,57 @@ void Square::updatePosition(p6::Context& ctx)
 {
     double dt = 1.0 / 60;
 
-    if (abs(this->center.x + this->radius) < 2 && abs(this->center.y + this->radius) < 1)
+    // Move the square in a random direction
+    int direction_x = rand() % 2 == 0 ? -1 : 1;
+    int direction_y = rand() % 2 == 0 ? -1 : 1;
+
+    double speed_factor = 1.0;
+
+    // Randomly change direction and speed up or slow down
+    if (rand() % 12 == 0)
     {
-        // Move the square in a random direction
-        int direction_x = rand() % 2 == 0 ? -1 : 1;
-        int direction_y = rand() % 2 == 0 ? -1 : 1;
-
-        double speed_factor = 1.0;
-
-        // Randomly change direction and speed up or slow down
-        if (rand() % 12 == 0)
-        {
-            direction_x  = -1;
-            speed_factor = rand() % 3 + 1;
-        }
-        else if (rand() % 12 == 1)
-        {
-            direction_y  = -1;
-            speed_factor = rand() % 3 + 1;
-        }
-        else if (rand() % 12 == 2)
-        {
-            direction_x  = 1;
-            speed_factor = rand() % 3 + 1;
-        }
-        else if (rand() % 12 == 3)
-        {
-            direction_y  = 1;
-            speed_factor = rand() % 3 + 1;
-        }
-
-        this->speed.x += direction_x * this->acc.x * dt;
-        this->speed.y += direction_y * this->acc.y * dt;
-        this->center.x += this->speed.x * dt;
-        this->center.y += this->speed.y * dt;
-
-        // If the square hits the left or right wall, change direction and slow down
-
-        if (this->center.x - this->radius < -2)
-        {
-            this->center.x = -2 + this->radius;
-            this->speed.x  = abs(this->speed.x);
-        }
-        else if (this->center.x + this->radius > 2)
-        {
-            this->center.x = 2 - this->radius;
-            this->speed.x  = -abs(this->speed.x);
-        }
-
-        if (this->center.y - this->radius < -1)
-        {
-            this->center.y = -1 + this->radius;
-            this->speed.y  = abs(this->speed.y);
-        }
-        else if (this->center.y + this->radius > 1)
-        {
-            this->center.y = 1 - this->radius;
-            this->speed.y  = -abs(this->speed.y);
-        }
+        direction_x = -1;
     }
-   
+    else if (rand() % 12 == 1)
+    {
+        direction_y = -1;
+    }
+    else if (rand() % 12 == 2)
+    {
+        direction_x = 1;
+    }
+    else if (rand() % 12 == 3)
+    {
+        direction_y = 1;
+    }
+
+    this->speed.x += direction_x * this->acc.x * dt;
+    this->speed.y += direction_y * this->acc.y * dt;
+    this->center.x += this->speed.x * dt;
+    this->center.y += this->speed.y * dt;
+
+    // If the square hits the walls, change direction and slow down
+
+    if (this->center.x - this->radius < -2)
+    {
+        this->center.x = -2 + this->radius;
+        this->speed.x  = abs(this->speed.x);
+    }
+
+    else if (this->center.x + this->radius > 2)
+    {
+        this->center.x = 2 - this->radius;
+        this->speed.x  = -abs(this->speed.x);
+    }
+
+    if (this->center.y - this->radius < -1)
+    {
+        this->center.y = -1 + this->radius;
+        this->speed.y  = abs(this->speed.y);
+    }
+    else if (this->center.y + this->radius > 1)
+    {
+        this->center.y = 1 - this->radius;
+        this->speed.y  = -abs(this->speed.y);
+    }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
 #include <vector>
 #include "glm/fwd.hpp"
+#include "imgui.h"
 #include "p6/p6.h"
 #define DOCTEST_CONFIG_IMPLEMENT
 #include "Square.hpp"
@@ -24,9 +25,11 @@ int main(int argc, char* argv[])
     /*********************************
      * HERE SHOULD COME THE INITIALIZATION CODE
      *********************************/
+    int                 squareNumber    = 50;
+    int                 maxSquareNumber = 100;
     std::vector<Square> boids;
 
-    for (int i = 0; i < 50; i++)
+    for (int i = 0; i < maxSquareNumber; i++)
     {
         Square square(
             glm::vec2(p6::random::number(-2.f, 2.f), p6::random::number(-1.f, 1.f)),
@@ -41,11 +44,20 @@ int main(int argc, char* argv[])
      * HERE SHOULD COME THE RENDERING CODE
      *********************************/
 
+    ctx.imgui = [&]() {
+        // Window
+        ImGui::Begin("Test");
+        ImGui::SliderInt("Square number", &squareNumber, 10, 100);
+        ImGui::SliderFloat("Square speed x", &boids[1].speed.x, -0.5f, 0.5f);
+        ImGui::SliderFloat("Square speed y", &boids[1].speed.y, -0.5f, 0.5f);
+        ImGui::End();
+    };
+
     // Declare your infinite update loop.
     ctx.update = [&]() {
         ctx.background(p6::NamedColor::RaspberryGlace);
 
-        for (int j = 0; j < 50; j++)
+        for (int j = 0; j < squareNumber; j++)
         {
             drawSquare(boids[j], ctx);
             boids[j].updatePosition(ctx);
