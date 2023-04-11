d4d2b614e95a674b2d67ded70c78c7e9de90db66
Charline <charli.le.pape@gmail.com>
2023-04-11 23:04:45 +0200

    reglage acc + fixe rebond

--- src/Square.cpp
+++ src/Square.cpp
@@ -17,24 +17,24 @@ void Square::restrictArea()
     if (this->_center.x - this->_radius < -2)
     {
         this->_center.x = -2 + this->_radius;
-        this->_speed.x  = abs(this->_speed.x);
+        this->_speed.x  = -this->_speed.x;
     }
 
     else if (this->_center.x + this->_radius > 2)
     {
         this->_center.x = 2 - this->_radius;
-        this->_speed.x  = -abs(this->_speed.x);
+        this->_speed.x  = -this->_speed.x;
     }
 
     if (this->_center.y - this->_radius < -1)
     {
         this->_center.y = -1 + this->_radius;
-        this->_speed.y  = abs(this->_speed.y);
+        this->_speed.y  = -this->_speed.y;
     }
     else if (this->_center.y + this->_radius > 1)
     {
         this->_center.y = 1 - this->_radius;
-        this->_speed.y  = -abs(this->_speed.y);
+        this->_speed.y  = -this->_speed.y;
     }
 }
 
@@ -82,19 +82,19 @@ void Square::updateAcc(std::vector<Square> boids, unsigned int i)
             continue;
         }
 
-        // on peut mettre aussi un critère de distance maximale:
-        if (distance > 0.3) {
-            continue;
-        }
-        
-
         glm::vec2 direction = (neighbour->_center - this->_center)/distance;
-        // comportement général grace  a une courbe attirance et séparation
-        glm::vec2 force = direction*(float)(-0.02/(distance+0.01)+0.02);
+        
+        float factorAttraction = 0.0004;
+        glm::vec2 attraction = direction*factorAttraction;
 
-        acc += force;
 
-        if (distance < 0.2) {  // distance minimale à régler.
+        float factorRepulsion = -0.0002;
+        glm::vec2 repulsion = direction*(1/(distance*distance))*factorRepulsion;
+
+
+        acc += attraction+repulsion;
+
+        if (distance < 0.7) {  // distance minimale à régler.
             // on écarte ceux qui sont trop loin
             sumSpeed = sumSpeed + neighbour->_speed;
             numspeedboids += 1;
@@ -105,10 +105,9 @@ void Square::updateAcc(std::vector<Square> boids, unsigned int i)
     // ajustement par rapport à la vitesse des voisins:
     if (numspeedboids!=0){
         glm::vec2 BoidsSpeedMean = sumSpeed / (float)numspeedboids;
-        //acc = acc + BoidsSpeedMean*(float)0.1; // paramètre à régler.
+        acc = acc + BoidsSpeedMean*(float)1.; // paramètre à régler.
     }
-    
-
+ 
     this->_acc = acc;
 }
 
