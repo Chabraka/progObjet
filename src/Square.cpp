6ca18d859ab861e0d649147569ca8c98834742a4
Charline <charli.le.pape@gmail.com>
2023-04-12 19:09:41 +0200

    try add squaretracker

--- src/Square.cpp
+++ src/Square.cpp
@@ -1,36 +1,46 @@
 #include "Square.hpp"
 #include <sys/types.h>
 
+/* ----- Draw ----- */
 void drawSquare(Square sqr, p6::Context& ctx)
 {
     ctx.square(
-        p6::Center{sqr._center.x, sqr._center.y},
-        p6::Radius{sqr._radius}
+        p6::Center{sqr.getCenter().x, sqr.getCenter().y},
+        p6::Radius{sqr.getRadius()}
     );
     ctx.fill = {1.f, 0.7f, 0.2f};
 }
 
+// rajouter mouvement aléatoire?
+
+/* ----- Restrictions ----- */
+
 void Square::restrictArea()
 {
-    // If the square hits the walls, change direction and slow down
+    /* If the square hits the walls, change direction */
 
+    // Left wall
     if (this->_center.x - this->_radius < -2)
     {
         this->_center.x = -2 + this->_radius;
         this->_speed.x  = -this->_speed.x;
     }
 
+    // Right wall
     else if (this->_center.x + this->_radius > 2)
     {
         this->_center.x = 2 - this->_radius;
         this->_speed.x  = -this->_speed.x;
     }
 
+    // Bottom wall
     if (this->_center.y - this->_radius < -1)
     {
         this->_center.y = -1 + this->_radius;
         this->_speed.y  = -this->_speed.y;
     }
+
+    // Top wall
     else if (this->_center.y + this->_radius > 1)
     {
         this->_center.y = 1 - this->_radius;
@@ -38,35 +48,49 @@ void Square::restrictArea()
     }
 }
 
+void Square::restrictSpeed()
+{
+    // max speed and min speed
+    float currentSpeed = glm::length(this->_speed);
+
+    // the speed need to be under 0
+    if (currentSpeed > 0.001)
+    {
+        if (currentSpeed > _maxSpeed)
+        {
+            this->_speed = glm::normalize(this->_speed) * _maxSpeed;
+        }
+        if (currentSpeed < _minSpeed)
+        {
+            this->_speed = glm::normalize(this->_speed) * _minSpeed;
+        }
+    }
+}
+
+/* ----- Updates ----- */
+
 void Square::updatePosition()
 {
     double dt = 1.0 / 60;
 
-    this->_center.x += this->_speed.x * dt + this->_acc.x * dt * dt / 2;
-    this->_center.y += this->_speed.y * dt + this->_acc.y * dt * dt / 2;
+    // Calcul of the position
+    this->_center.x += this->_speed.x * dt + this->_acceleration.x * dt * dt / 2;
+    this->_center.y += this->_speed.y * dt + this->_acceleration.y * dt * dt / 2;
 
-    this->_speed.x += this->_acc.x * dt;
-    this->_speed.y += this->_acc.y * dt;
+    // Calcul of the speed
+    this->_speed.x += this->_acceleration.x * dt;
+    this->_speed.y += this->_acceleration.y * dt;
 
-    // max speed
-
-    float currentSpeed = glm::length(this->_speed);
-    if (currentSpeed > _maxSpeed)
-    {
-        this->_speed = glm::normalize(this->_speed) * _maxSpeed;
-    }
-    /*if (currentSpeed < _minSpeed)
-    {
-        this->_speed = glm::normalize(this->_speed) * _minSpeed;
-    }*/
+    // Restrict the position
+    this->restrictSpeed();
     this->restrictArea();
 }
 
 void Square::updateAcc(std::vector<Square> boids, unsigned int i)
 {
     glm::vec2 acc(0., 0.);
-    glm::vec2 sumSpeed(0., 0.);  // somme des vitesses des voisins
-    int numspeedboids = 0; // nombre de voisins concernés par l'ajustement de vitesse
+    glm::vec2 sumSpeed(0., 0.);  // sum speed of neighbors
+    int       numspeedboids = 0; // number of neighbors concern by the speed adjustment
 
     for (unsigned int j = 0; j < boids.size(); j++)
     {
@@ -74,81 +98,76 @@ void Square::updateAcc(std::vector<Square> boids, unsigned int i)
         {
             continue;
         }
-
         Square* neighbour = &boids[j];
         float   distance  = glm::distance(this->_center, neighbour->_center);
         if (distance <= 0.001)
         {
             continue;
         }
-
-        glm::vec2 direction = (neighbour->_center - this->_center)/distance;
-        
-        float factorAttraction = 0.0004;
-        glm::vec2 attraction = direction*factorAttraction;
-
-
-        float factorRepulsion = -0.0002;
-        glm::vec2 repulsion = direction*(1/(distance*distance))*factorRepulsion;
-
-
-        acc += attraction+repulsion;
-
-        if (distance < 0.7) {  // distance minimale à régler.
-            // on écarte ceux qui sont trop loin
+        glm::vec2 direction = (neighbour->_center - this->_center) / distance;
+        acc += attraction(direction) + repulsion(direction, distance);
+        const float minDistance = 0.7;
+        if (distance < minDistance)
+        { // minimal distance to adjust 0.7 it's a big crowd, 0.2 you have little groups
+            // exclude those who are too far away
             sumSpeed = sumSpeed + neighbour->_speed;
             numspeedboids += 1;
         }
+    }
 
-        
-    }
-    // ajustement par rapport à la vitesse des voisins:
-    if (numspeedboids!=0){
-        glm::vec2 BoidsSpeedMean = sumSpeed / (float)numspeedboids;
-        acc = acc + BoidsSpeedMean*(float)1.; // paramètre à régler.
-    }
- 
-    this->_acc = acc;
+    this->_acceleration = adjustSpeed(acc, sumSpeed, numspeedboids);
 }
 
+/* ----- Behaviors ----- */
 
-
-
-
-/*void Square::checkCollision(std::vector<Square> boids, unsigned int i)
+glm::vec2 attraction(glm::vec2 direction)
 {
-    // rajouter élément dans classe square
-    for (unsigned int j = 0; j < boids.size(); j++)
-    {
-        if (i == j)
-        {
-            continue;
-        }
-        Square* neighbour = &boids[j];
-        float   distance  = glm::distance(this->_center, neighbour->_center);
+    const float factorAttraction = 0.0004;
+    return direction * factorAttraction;
+}
 
-        float minDistance = this->_radius * 2.f;
-        if (distance < minDistance)
-        {
-            glm::vec2 direction   = (this->_center - neighbour->_center) / distance;
-            float     penetration = minDistance - distance;
-            this->_center += direction * penetration * 0.5f;
-            neighbour->_center -= direction * penetration * 0.5f;
-            this->_speed -= direction * glm::dot(this->_speed, direction) * 0.5f;
-            neighbour->_speed -= direction * glm::dot(neighbour->_speed, direction) * 0.5f;
-        }
+glm::vec2 repulsion(glm::vec2 direction, float distance)
+{
+    const float factorRepulsion = -0.0002;
+    float       maxRepulsion    = -3.;
+    return direction * std::max((1 / (distance * distance)) * factorRepulsion, maxRepulsion);
+}
+
+glm::vec2 adjustSpeed(glm::vec2 acc, glm::vec2 sumSpeed, int numspeedboids)
+{
+    // Adjustment of the speed relative to the neighbors
+    if (numspeedboids != 0)
+    {
+        glm::vec2 BoidsSpeedMean = sumSpeed / (float)numspeedboids;
+        acc                      = acc + BoidsSpeedMean * (float)1.; // parameter to adjust (1) can be a factorSpeedMean
     }
+    return acc;
+}
+
+/*void Square::trackSquare(std::vector<Square> boids, unsigned int i)
+{
+    Square square(
+        glm::vec2(p6::random::number(-2.f, 2.f), p6::random::number(-1.f, 1.f)),
+        0.05f,
+        glm::vec2(p6::random::number(-0.02f, 0.02f), p6::random::number(-0.02f, 0.02f)),
+        glm::vec2(0., 0.),
+        0.3,
+        0.1
+    );
+
+    drawSquare(tracker, ctx);
+
+    const float factorAttraction = 0.001;
+
 }*/
 
+/* ----- Boids ----- */
 
 void updateBoidsAcc(std::vector<Square>* boids)
 {
     for (unsigned int i = 0; i < boids->size(); i++)
     {
         boids->at(i).updateAcc(*boids, i);
-        //boids->at(i).checkCollision(*boids, i);
     }
 }
-
-
 
